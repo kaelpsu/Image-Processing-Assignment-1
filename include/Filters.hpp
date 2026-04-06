aca8929390d542b8e4c762c42ceb880f8eb5ef6b
Kael Paraguassu <kaelparaguassu@hotmail.com>
2026-04-06 15:42:45 -0300

    feat: add high and lowpass filters

---
+++ include/Filters.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <vector>
+
+std::vector<std::vector<float>> lowPass3x3();
+std::vector<std::vector<float>> highPass3x3();
---
+++ src/Filters.cpp
@@ -0,0 +1,17 @@
+#include "Filters.hpp"
+
+std::vector<std::vector<float>> lowPass3x3() {
+    return {
+        {1/9.0, 1/9.0, 1/9.0},
+        {1/9.0, 1/9.0, 1/9.0},
+        {1/9.0, 1/9.0, 1/9.0}
+    };
+}
+
+std::vector<std::vector<float>> highPass3x3() {
+    return {
+        {-1, -1, -1},
+        {-1,  8, -1},
+        {-1, -1, -1}
+    };
+}
