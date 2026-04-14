60ab7bce9d099561a87c0297f8ecf8b1b43c5b26
Kael Paraguassu <kaelparaguassu@hotmail.com>
2026-04-14 10:15:00 -0300

    feat: add file name argument to q2 and q3

--- assignments/q2-convolution.cpp
+++ assignments/q2-convolution.cpp
@@ -3,22 +3,54 @@
 #include "processing/Convolution.hpp"
 #include "io/ImageIO.hpp"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    // leitura
-    Image image = readImage("images/input/Google_JAX_low_contrast.pgm");
+int main(int argc, char* argv[]) {
 
-    // filtros
-    Image lowResult = convolve(image, lowPass3x3());
-    Image highResult = convolve(image, highPass3x3());
+    if (argc != 2) {
+        cerr << "Erro: Número incorreto de argumentos.\n";
+        cerr << "Uso correto: " << argv[0] << " <input_file>\n";
+        return 1;
+    }
 
-    // saída
-    writeImage("images/output/lowpass.pgm", lowResult);
-    writeImage("images/output/highpass.pgm", highResult);
+    string inputFile = argv[1];
+    string outputFile = inputFile;
 
-    cout << "Processamento concluido!\n";
+    // Extract only the filename
+    size_t slashPos = inputFile.find_last_of("/\\");
+    string pureFilename = (slashPos == string::npos) ? inputFile : inputFile.substr(slashPos + 1);
+
+    // 2. Separate the base name from the extension
+    size_t dotPos = pureFilename.find_last_of(".");
+    string baseName = (dotPos == string::npos) ? pureFilename : pureFilename.substr(0, dotPos);
+    string extension = ".pgm"; // default extension
+
+    string outDir = "images/output/";
+    string outLow = outDir + baseName + "_lowpass" + extension;
+    string outHigh = outDir + baseName + "_highpass" + extension;
+
+    try {
+        cout << "Lendo imagem: " << inputFile << "...\n";
+        Image image = readImage(inputFile);
+
+        cout << "Aplicando filtros...\n";
+        Image lowResult = convolve(image, lowPass3x3());
+        Image highResult = convolve(image, highPass3x3());
+
+        cout << "Salvando resultados...\n";
+        writeImage(outLow, lowResult);
+        writeImage(outHigh, highResult);
+
+        cout << "Processamento concluido!\n";
+        cout << "-> " << outLow << "\n";
+        cout << "-> " << outHigh << "\n";
+
+    } catch (const exception& e) {
+        cerr << "Erro: " << e.what() << "\n";
+        return 1;
+    }
 
     return 0;
 }
--- assignments/q3-histogram.cpp
+++ assignments/q3-histogram.cpp
@@ -5,14 +5,45 @@
 
 using namespace std;
 
-int main() {
-    Image img = readImage("images/input/Hill.png");
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cerr << "Erro: Número incorreto de argumentos.\n";
+        cerr << "Uso correto: " << argv[0] << " <input_file>\n";
+        return 1;
+    }
 
-    Image result = histogramEqualization(img);
+    string inputFile = argv[1];
+    string outputFile = inputFile;
 
-    writeImage("images/output/equalized.pgm", result);
+    // Extract only the filename
+    size_t slashPos = inputFile.find_last_of("/\\");
+    string pureFilename = (slashPos == string::npos) ? inputFile : inputFile.substr(slashPos + 1);
 
-    cout << "Processamento concluido!\n";
+    // 2. Separate the base name from the extension
+    size_t dotPos = pureFilename.find_last_of(".");
+    string baseName = (dotPos == string::npos) ? pureFilename : pureFilename.substr(0, dotPos);
+    string extension = ".pgm"; // default extension
+
+    string outDir = "images/output/";
+    string out = outDir + baseName + "_equalized" + extension;
+
+    try {
+        cout << "Lendo imagem: " << inputFile << "...\n";
+        Image img = readImage(inputFile);
+
+        cout << "Equalizando histograma...\n";
+        Image result = histogramEqualization(img);
+
+        cout << "Salvando resultado...\n";
+        writeImage(out, result);
+
+        cout << "Processamento concluido!\n";
+        cout << "-> " << out << "\n";
+    
+    } catch (const exception& e) {
+        cerr << "Erro: " << e.what() << "\n";
+        return 1;
+    }
 
     return 0;
 }
